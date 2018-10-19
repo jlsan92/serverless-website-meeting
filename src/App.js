ea6fe8f7f8d381ba4553320010463f9a3528a408
Juan Sanchez <juan.sanchez1192@gmail.com>
2018-10-19 10:28:56 +0200

    Testing PRs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello there</h1>
+          <h1 className="App-title">This is a feature <span role="img" aria-label="emoji">😁</span></h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
